a39e432addbda619fe2a9c5659a3249d753e1587
Ilia <polikarpovilja@mail.ru>
2022-11-25 19:17:48 +0300

    Дз 3

---
+++ Dz/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+double X1 = UserInput("Введите координату X1: ");
+
+double Y1 = UserInput("Введите координату Y1: ");
+
+double Z1 = UserInput("Введите координату Z1: ");
+
+double X2 = UserInput("Введите координату X2: ");
+
+double Y2 = UserInput("Введите координату Y2: ");
+
+double Z2 = UserInput("Введите координату Z2: ");
+
+double res = DisPoint (X1, Y1, X2, Y2, Z1, Z2);
+Console.WriteLine($"{res}");
+
+static double DisPoint (double X1, double Y1, double X2, double Y2, double Z1, double Z2)
+{
+    return Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
+}
+
+static double UserInput (string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine() ?? "");
+}     
---
+++ dz19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите пятизначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10000 && num < 100000)
+{
+  int a = num / 10000;
+  int b = num % 10000 / 1000;
+  int c = num % 1000 / 100;
+  int d = num % 100 / 10;
+  int e = num % 10;
+   if (a == e && b == d)
+   { 
+     Console.Write("Палидром");
+    }
+    else
+    {
+        Console.Write("не палидром ");
+    }
+}
+    else Console.WriteLine("Введенное число не является пятизначным");
