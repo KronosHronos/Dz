57d22ba65f101c2247b4c45a05ab554e59b543eb
Ilia <polikarpovilja@mail.ru>
2022-11-25 19:26:13 +0300

    Dz

---
+++ dz 23/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int i= 1;
+int N = UserInt("Введите число N: ");
+ Cube(N);
+ 
+void Cube(int Number)
+{
+    while ( i<= Number)
+    {
+    Console.WriteLine($"{i}^3 = {i*i*i}");
+    i= i + 1;
+    }
+}
+static int UserInt(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
